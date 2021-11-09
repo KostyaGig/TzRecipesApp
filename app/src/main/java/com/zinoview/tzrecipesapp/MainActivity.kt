2206965d2699160a50a3acdd5c3110199e3fead4
kostyagig <k.gig@list.ru>
2021-11-09 09:01:19 +0300

    Initial commit

---
+++ app/src/main/java/com/zinoview/tzrecipesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zinoview.tzrecipesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
